7e36a1253d777093f315c1cec5b03154d73c541d
25GUTY <rjoel887@outlook.com>
2026-06-21 22:16:36 -0600

    Listar y Guardar EmpleadoController.php

--- sistema_crud/grupo1/gestion-empleados/controllers/EmpleadoController.php
+++ sistema_crud/grupo1/gestion-empleados/controllers/EmpleadoController.php
@@ -1,1 +1,31 @@
+<?php
+require_once 'config/conexion.php';
 
+class EmpleadoController {
+    
+    public static function listar() {
+       global $conexion;
+        $stmt = $conexion->prepare("SELECT * FROM empleados");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function guardar($nombre, $puesto, $salario) {
+        global $conexion;
+        $stmt = $conexion->prepare("INSERT INTO empleados (nombre, puesto, salario) VALUES (:nombre, :puesto, :salario)");
+        $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+        $stmt->bindParam(':puesto', $puesto, PDO::PARAM_STR);
+        $stmt->bindParam(':salario', $salario, PDO::PARAM_STR);
+        
+       $stmt->execute();
+        header("Location: index.php");
+        exit;
+    }
+
+    
+
+    
+
+    
+}
+?>
