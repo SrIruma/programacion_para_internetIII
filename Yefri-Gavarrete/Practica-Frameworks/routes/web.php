838fa908c63c99e1651234303b067234c763ac05
Yefri Gavarrete <130026945+YefriGavarrete@users.noreply.github.com>
2026-07-06 21:43:16 -0600

    Modificación de index + correción de errores

--- Yefri-Gavarrete/Practica-Frameworks/routes/web.php
+++ Yefri-Gavarrete/Practica-Frameworks/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/public/index.html', function () {
-    return view('index');
+Route::get('/', function () {
+    return redirect('/index.html');
 });
